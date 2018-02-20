3dff841f0218e961d9f4440e5c3c7c0780990c78
jgn <jan.gonera@infullmobile.com>
2018-02-20 15:51:48 +0100

    displaying empty viewholders for results

---
+++ app/src/main/java/org/rhinoonabus/stackoverflowbrowser/presentation/search/SearchResultsAdapter.kt
@@ -0,0 +1,7 @@
+package org.rhinoonabus.stackoverflowbrowser.presentation.search
+
+/**
+ * Created by JanGonera on 20/02/2018.
+ */
+
+class SearchResultsAdapter
---
+++ app/src/test/java/org/rhinoonabus/stackoverflowbrowser/presentation/search/SearchResultsAdapterTest.kt
@@ -0,0 +1,8 @@
+package org.rhinoonabus.stackoverflowbrowser.presentation.search
+
+import org.mockito.Mockito.*
+
+/**
+ * Created by JanGonera on 20/02/2018.
+ */
+class SearchResultsAdapterTest
